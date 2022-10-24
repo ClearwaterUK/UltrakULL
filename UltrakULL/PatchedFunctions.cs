034341325ef272ab573580c8a95e42f0a7dcd88c
Daniel Murray <21901746@etu.unicaen.fr>
2022-10-24 13:52:00 +0200

    Fixed the "transaction complete" HUD message from 4-S appearing at the end of every level.

--- UltrakULL/PatchedFunctions.cs
+++ UltrakULL/PatchedFunctions.cs
@@ -41,19 +41,22 @@ namespace UltrakULL
         //Overrides the private CoinsToPoints function in the CrateCounter class
         public static bool CoinsToPoints_MyPatch(CrateCounter __instance, int ___savedCoins)
         {
-            GameProgressSaver.AddMoney(___savedCoins * 100);
-
-            MonoSingleton<HudMessageReceiver>.Instance.SendHudMessage(string.Concat(new object[]
+            if (SceneManager.GetActiveScene().name == "Level 4-S")
             {
-                "<color=grey>" + LanguageManager.CurrentLanguage.act2.act2_greedSecret_transactionComplete1 + ":</color> ",
-                ___savedCoins,
-                " " + LanguageManager.CurrentLanguage.act2.act2_greedSecret_transactionComplete2 +" <color=orange>=></color> ",
-                StatsManager.DivideMoney(___savedCoins * 100),
-                "<color=orange>P</color>"
-            }), "", "", 0, false);
+                GameProgressSaver.AddMoney(___savedCoins * 100);
+
+                MonoSingleton<HudMessageReceiver>.Instance.SendHudMessage(string.Concat(new object[]
+                {
+                    "<color=grey>" + LanguageManager.CurrentLanguage.act2.act2_greedSecret_transactionComplete1 + ":</color> ",
+                    ___savedCoins,
+                    " " + LanguageManager.CurrentLanguage.act2.act2_greedSecret_transactionComplete2 +" <color=orange>=></color> ",
+                    StatsManager.DivideMoney(___savedCoins * 100),
+                    "<color=orange>P</color>"
+                }), "", "", 0, false);
 
 
             ___savedCoins = 0;
+            }
             return false;
         }
 
