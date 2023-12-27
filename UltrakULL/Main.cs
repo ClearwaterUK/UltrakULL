fc5bfae13d704facbe4cafc0a8a5d60432df2555
Clearwater <xxx@xxx.com>
2023-12-27 19:25:17 +0100

    Version increment

--- UltrakULL/Main.cs
+++ UltrakULL/Main.cs
@@ -55,7 +55,7 @@ namespace UltrakULL
     {
         private const string Guid = "clearwater.ultrakill.ultrakull";
         private const string InternalName = "clearwater.ultrakull.ultrakULL";
-        private const string InternalVersion = "1.2.3";
+        private const string InternalVersion = "1.2.4";
 
         public static MainPatch Instance;
         public bool ready;
