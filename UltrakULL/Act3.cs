b4246cfc1678f0f60aae4e9f66c9825f6d173b9d
FrizouWasThere <101941926+FrizouWasThere@users.noreply.github.com>
2023-12-31 15:58:01 -0500

    7-3 Secret and made it so the 7-4 patch only applies in 7-4

--- UltrakULL/Act3.cs
+++ UltrakULL/Act3.cs
@@ -32,15 +32,34 @@ namespace UltrakULL
             
             PatchResultsScreen(levelName, levelChallenge);
             PatchHellmap(ref canvasObj);
-            
-            //7-4 Flooding and Countdown
-            //Flooding warning
-            GameObject floodingWarnText = GetTextMeshProUGUI(GetGameObjectChild(GetGameObjectChild(canvasObj, "Warning"), "Text (TMP)"));
-            floodingWarnText.text = LanguageManager.CurrentLanguage.act3.act3_floodingWarning;
 
-            //Countdown title before blowing up
-            GameObject countdownTitleText = GetTextMeshProUGUI(GetGameObjectChild(GetGameObjectChild(canvasObj, "Countdown"), "Text (TMP)"));
-            countdownTitleText.text = LanguageManager.CurrentLanguage.act3.act3_countdownTitle;
+            //You're the star of the show now, baby!
+            if (currentLevel.Contains("7-3"))
+            {
+                GameObject secretScreenArea = GetGameObjectChild(GetInactiveRootObject("Main Section"), "8 - Upper Garden Battlefield");
+                GameObject secretScreen = GetGameObjectChild(GetGameObjectChild(GetGameObjectChild(GetGameObjectChild(secretScreenArea, "8 Stuff(Clone)"), "Destructible Tunnel"), "Puzzle Screen"), "Canvas");
+
+                TextMeshProUGUI becomeMarked = GetTextMeshProUGUI(GetGameObjectChild(GetGameObjectChild(secretScreen, "PreActivation"), "Text (TMP) (1)"));
+                TextMeshProUGUI becomeMarkedButton = GetTextMeshProUGUI(GetGameObjectChild(GetGameObjectChild(GetGameObjectChild(GetGameObjectChild(secretScreen, "PreActivation"), "Button A"), "On"), "Text"));
+                TextMeshProUGUI becomeMarkedButtonClosed = GetTextMeshProUGUI(GetGameObjectChild(GetGameObjectChild(GetGameObjectChild(GetGameObjectChild(secretScreen, "PreActivation"), "Button A"), "Off"), "Text"));
+                TextMeshProUGUI starOfTheShow = GetTextMeshProUGUI(GetGameObjectChild(GetGameObjectChild(secretScreen, "PostActivation"), "Text (TMP) (1)"));
+
+                becomeMarked.text = LanguageManager.CurrentLanguage.act3.act3_third_becomeMarked;
+                becomeMarkedButton.text = LanguageManager.CurrentLanguage.act3.act3_third_becomeMarkedButton;
+                becomeMarkedButtonClosed.text = LanguageManager.CurrentLanguage.act3.act3_third_becomeMarkedButtonClosed;
+                starOfTheShow.text = LanguageManager.CurrentLanguage.act3.act3_third_starOfTheShow;
+            }
+            //7-4 Flooding and Countdown
+            else if (currentLevel.Contains("7-4"))
+            {
+                //Flooding warning
+                GameObject floodingWarnText = GetTextMeshProUGUI(GetGameObjectChild(GetGameObjectChild(canvasObj, "Warning"), "Text (TMP)"));
+                floodingWarnText.text = LanguageManager.CurrentLanguage.act3.act3_floodingWarning;
+
+                //Countdown title before blowing up
+                GameObject countdownTitleText = GetTextMeshProUGUI(GetGameObjectChild(GetGameObjectChild(canvasObj, "Countdown"), "Text (TMP)"));
+                countdownTitleText.text = LanguageManager.CurrentLanguage.act3.act3_countdownTitle;
+            }
         }
     }
 }
