802e069fcac030fc180b62525e68b134e8dc11cd
FrizouWasThere <101941926+FrizouWasThere@users.noreply.github.com>
2024-01-13 14:12:32 -0500

    Update SandboxPatches.cs

--- UltrakULL/Harmony Patches/SandboxPatches.cs
+++ UltrakULL/Harmony Patches/SandboxPatches.cs
@@ -86,6 +86,36 @@ namespace UltrakULL.Harmony_Patches
         }
     }
     
+    [HarmonyPatch(typeof(AlterMenuElements),"CreateTitle")]
+    public static class SandboxAlterOptionsTitles
+    {
+        [HarmonyPrefix]
+        public static bool sandboxAlterOptionsTitles_Prefix(ref string name)
+        {
+            if(isUsingEnglish())
+            {
+                return true;
+            }
+            switch (name)
+            {
+                case "enemy": { name = LanguageManager.CurrentLanguage.misc.enemyAlter_boss_title; break;}
+                case "Jump Pad": { name = LanguageManager.CurrentLanguage.misc.enemyAlter_jumpPadTitle; break;}
+                case "Hook Point": { name = LanguageManager.CurrentLanguage.misc.enemyAlter_hookPointTitle; break;}
+                case "Breakable": { name = LanguageManager.CurrentLanguage.misc.enemyAlter_metaBreakable; break; }
+                case "Sandbox": { name = LanguageManager.CurrentLanguage.misc.enemyAlter_sandboxTitle; break; }
+                case "v2": { name = LanguageManager.CurrentLanguage.enemyNames.enemyname_v2; break;}
+                case "swordsmachine": { name = LanguageManager.CurrentLanguage.enemyNames.enemyname_swordsmachine; break;}
+                case "drone": { name = LanguageManager.CurrentLanguage.enemyNames.enemyname_virtue; break;}
+                case "statue": case "cerberus": { name = LanguageManager.CurrentLanguage.enemyNames.enemyname_cerberus; break;}
+                case "mindflayer": { name = LanguageManager.CurrentLanguage.enemyNames.enemyname_mindFlayer; break;}
+                case "malicious face": { name = LanguageManager.CurrentLanguage.enemyNames.enemyname_malFace; break;}
+                default:{break;}
+                    
+            }
+            return true;
+        }
+    }
+    
     [HarmonyPatch(typeof(AlterMenuElements),"CreateBoolRow")]
     public static class SandboxAlterOptionsBoxes
     {
@@ -104,9 +134,11 @@ namespace UltrakULL.Harmony_Patches
                 case "Sandified": { name = LanguageManager.CurrentLanguage.misc.enemyAlter_sandified; break;}
                 case "Ignore Player": { name = LanguageManager.CurrentLanguage.misc.enemyAlter_ignorePlayer; break;}
                 case "Attack Enemies": { name = LanguageManager.CurrentLanguage.misc.enemyAlter_attackEnemies; break;}
-                case "Force": { name = LanguageManager.CurrentLanguage.misc.enemyAlter_Power; break;}
+                case "Force": { name = LanguageManager.CurrentLanguage.misc.enemyAlter_power; break;}
                 case "Weak": { name = LanguageManager.CurrentLanguage.misc.enemyAlter_metaWeak; break;}
-                case "Unbreakable": { name = LanguageManager.CurrentLanguage.misc.enemyAlter_metaUnbreakable; break;}
+                case "Unbreakable": { name = LanguageManager.CurrentLanguage.misc.enemyAlter_metaUnbreakable; break; }
+                case "Has Skull": { name = LanguageManager.CurrentLanguage.misc.enemyAlter_hasSkull; break; }
+                case "Altar Type": { name = LanguageManager.CurrentLanguage.misc.enemyAlter_altarType; break; }
                 default:{break;}
                     
             }
