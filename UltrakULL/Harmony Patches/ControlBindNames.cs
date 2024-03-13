7594abc7fb5e4ac9dceffe3f78dec6eea85c7b14
FrizouWasThere <101941926+FrizouWasThere@users.noreply.github.com>
2024-03-12 20:35:09 -0400

    Update ControlBindNames.cs

--- UltrakULL/Harmony Patches/ControlBindNames.cs
+++ UltrakULL/Harmony Patches/ControlBindNames.cs
@@ -59,16 +59,11 @@ namespace UltrakULL.Harmony_Patches
                 case "PRIMARY FIRE": { return LanguageManager.CurrentLanguage.options.controls_primaryFire; }
                 case "SECONDARY FIRE": { return LanguageManager.CurrentLanguage.options.controls_secondaryFire; }
                 case "CHANGE VARIATION": { return LanguageManager.CurrentLanguage.options.controls_changeVariation; }
-                case "SLOT 0": { return LanguageManager.CurrentLanguage.options.controls_slot0; }
-                case "SLOT 1": { return LanguageManager.CurrentLanguage.options.controls_slot1; }
-                case "SLOT 2": { return LanguageManager.CurrentLanguage.options.controls_slot2; }
-                case "SLOT 3": { return LanguageManager.CurrentLanguage.options.controls_slot3; }
-                case "SLOT 4": { return LanguageManager.CurrentLanguage.options.controls_slot4; }
-                case "SLOT 5": { return LanguageManager.CurrentLanguage.options.controls_slot5; }
-                case "SLOT 6": { return LanguageManager.CurrentLanguage.options.controls_slot6; }
-                case "SLOT 7": { return LanguageManager.CurrentLanguage.options.controls_slot7; }
-                case "SLOT 8": { return LanguageManager.CurrentLanguage.options.controls_slot8; }
-                case "SLOT 9": { return LanguageManager.CurrentLanguage.options.controls_slot9; }
+                case "REVOLVER": { return LanguageManager.CurrentLanguage.options.controls_revolver; }
+                case "SHOTGUN": { return LanguageManager.CurrentLanguage.options.controls_shotgun; }
+                case "NAILGUN": { return LanguageManager.CurrentLanguage.options.controls_nailgun; }
+                case "RAILCANNON": { return LanguageManager.CurrentLanguage.options.controls_railcannon; }
+                case "ROCKET LAUNCHER": { return LanguageManager.CurrentLanguage.options.controls_rocketLauncher; }
                 case "NEXT WEAPON": { return LanguageManager.CurrentLanguage.options.controls_nextWeapon; }
                 case "PREVIOUS WEAPON": { return LanguageManager.CurrentLanguage.options.controls_previousWeapon; }
                 case "LAST WEAPON": { return LanguageManager.CurrentLanguage.options.controls_lastUsedWeapon; }
