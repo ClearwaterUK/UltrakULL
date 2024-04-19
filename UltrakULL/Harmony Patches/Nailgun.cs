a5c1e692395bdb49b90849ce26b3a1ac35cef1dc
Tsuchinoko <akaneminecraft@gmail.com>
2024-04-20 01:44:25 +0900

    i have no time

--- UltrakULL/Harmony Patches/Nailgun.cs
+++ UltrakULL/Harmony Patches/Nailgun.cs
@@ -11,6 +11,7 @@ using static UnityEngine.GraphicsBuffer;
 using TMPro;
 using System.IO;
 using System.Reflection;
+using UltrakULL.json;
 
 namespace UltrakULL.Harmony_Patches
 {
@@ -21,20 +22,14 @@ namespace UltrakULL.Harmony_Patches
         public static void NailgunPostfix(Nailgun __instance, TMP_Text ___statusText)
         {
             Thread.Sleep(3);
-            if (Core.GlobalFontReady)
+            if (Core.GlobalFontReady && __instance.gameObject.name.ToLower().Contains("Magnet"))
             {
                 __instance.ammoText.font = TextFontSwap.originalFont;
             }
-            /*if(__instance.gameObject.name.ToLower().Contains("zapper") && (___statusText != null))
-            {
-                GameObject zapperImage = ___statusText.transform.parent.gameObject;
-                TextMeshProUGUI rechargeText = GetTextMeshProUGUI(GetGameObjectChild(GetGameObjectChild(zapperImage, "ZapperRecharge"), "Text (TMP)"));
-                rechargeText.text = "充電中";
-            }*/
         }
 
         [HarmonyPatch("UpdateZapHud"), HarmonyPostfix]
-        public static void zapperpostfix(Nailgun __instance, Zapper ___currentZapper, CameraController ___cc, TMP_Text ___statusText)
+        public static void zapperpostfix(Nailgun __instance, Zapper ___currentZapper, CameraController ___cc, TMP_Text ___statusText, GameObject ___rechargingOverlay)
         {
             if (___currentZapper == null)
             {
@@ -42,8 +37,14 @@ namespace UltrakULL.Harmony_Patches
                 {
                     return;
                 }
+                if (___rechargingOverlay.activeSelf)
+                {
+                    TextMeshProUGUI rechargeText = GetTextMeshProUGUI(GetGameObjectChild(___rechargingOverlay, "Text (TMP)"));
+                    //rechargeText.text = LanguageManager.CurrentLanguage.
+                }
                 RaycastHit raycastHit;
                 EnemyIdentifierIdentifier enemyIdentifierIdentifier;
+                float target;
                 if (Physics.Raycast(___cc.GetDefaultPos(), ___cc.transform.forward, out raycastHit,
                     float.PositiveInfinity, LayerMaskDefaults.Get(LMD.EnemiesAndEnvironment)) &&
                     raycastHit.collider.gameObject.layer != 8 && raycastHit.collider.gameObject.layer != 24 &&
@@ -52,26 +53,27 @@ namespace UltrakULL.Harmony_Patches
                 {
                     if (raycastHit.distance < __instance.zapper.maxDistance - 5f)
                     {
+                        target = 1f - (__instance.zapper.maxDistance - 5f - raycastHit.distance) / (__instance.zapper.maxDistance - 5f);
                         ___statusText.text = "準備完了";
                     }
                     else
                     {
                         ___statusText.text = (__instance.altVersion ? "遠い" : "範囲外");
+                        target = 0f;
                     }
                 }
                 else
                 {
-                    Logging.Info("NMOONOMO");
+                    target = 0f;
                     ___statusText.text = (__instance.altVersion ? "なし" : "対象なし");
                 }
+                return;
             }
             if (___currentZapper.distance > ___currentZapper.maxDistance || ___currentZapper.raycastBlocked)
             {
-                Logging.Info("MONMO");
                 ___statusText.text = (___currentZapper.raycastBlocked ? "障害物" : (__instance.altVersion ? "遠い" : "範囲外"));
                 return;
             }
-            Logging.Info("NmONMO");
             ___statusText.text = (__instance.altVersion ? "" : "距離: ") + ___currentZapper.distance.ToString("f1");
         }
 
